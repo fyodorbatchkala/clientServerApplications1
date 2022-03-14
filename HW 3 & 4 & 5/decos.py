4d117c3a0e8be037e7b49c558df539223efe3cda
fbatch <fyodor.batchkala@gmail.com>
2022-03-14 09:33:21 +0000

    corrected HW6

--- HW 3 & 4 & 5/decos.py
+++ HW 3 & 4 & 5/decos.py
@@ -5,17 +5,20 @@ import logs.config_client_log
 import traceback
 import inspect
 
-
 if sys.argv[0].find('client.py') == -1:
+    LOGGER = logging.getLogger('server')
+else:
     LOGGER = logging.getLogger('client')
-    
+
 
 def log(func_to_log):
     def log_saver(*args, **kwargs):
         ret = func_to_log(*args, **kwargs)
-        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} с параметрами {args}, {kwargs}. '
+        LOGGER.debug(f'Была вызвана функция {func_to_log.__name__} c параметрами {args}, {kwargs}.'
                      f'Вызов из модуля {func_to_log.__module__}.'
                      f'Вызов из функции {traceback.format_stack()[0].strip().split()[-1]}.'
-                     f'Вызов из функции {inspect.stack()[1][3]}')
+                     f'Вызов из функции {inspect.stack()[1][3]}'
+                     f'@@@Вызов из функции {sys._getframe().f_back.f_code.co_name}'
+                     f'@@@вызов из модуля {sys._getframe().f_back.f_code.co_filename.split("/")[-1]}')
         return ret
-    return log_saver()
+    return log_saver
--- HW 3 & 4 & 5/server.py
+++ HW 3 & 4 & 5/server.py
@@ -1,99 +1,86 @@
-import sys
 import socket
+import sys
+import argparse
 import json
 import logging
-import argparse
-from errors import IncorrectDataReceivedError
 import logs.config_server_log
-from common.variables import DEFAULT_PORT, DEFAULT_IP_ADDRESS, MAX_CONNECTIONS, ENCODING, ACTION,\
-    TIME, USER, ACCOUNT_NAME, PRESENCE, RESPONSE, ERROR, RESPONDEFAULT_IP_ADDRESSSE
+from errors import IncorrectDataReceivedError
+from common.variables import ACTION, USER, ACCOUNT_NAME, PRESENCE, \
+    TIME, DEFAULT_PORT, MAX_CONNECTIONS, RESPONSE, ERROR
 from common.utils import get_message, send_message
+# from common.utils_for_log_as_class import get_message, send_message
+from decos import log
+
+# Инициализация логирования сервера.
+LOGGER = logging.getLogger('server')
 
 
-SERVER_LOGGER = (logging.getLogger('server'))
-
-
+@log
 def process_client_message(message):
-    SERVER_LOGGER.debug(f'разбор сообщения от клиента : {message}')
-    if ACTION in message and message[ACTION] == PRESENCE and TIME in message \
-            and USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
+    """
+    Обработчик сообщений от клиентов, принимает словарь - сообщение от клинта,
+    проверяет корректность, возвращает словарь-ответ для клиента
+    :param message:
+    :return:
+    """
+    LOGGER.debug(f'Разбор сообщения от клиента : {message}')
+    if ACTION in message and message[ACTION] == PRESENCE and TIME in message and \
+            USER in message and message[USER][ACCOUNT_NAME] == 'Guest':
         return {RESPONSE: 200}
     return {
-        RESPONDEFAULT_IP_ADDRESSSE: 400,
-        ERRЩК: 'Bad Request'
+        RESPONSE: 400,
+        ERROR: 'Bad Request'
     }
 
 
+@log
 def create_arg_parser():
+    """Парсер аргументов коммандной строки"""
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', default=DEFAULT_PORT, type=int, nargs='?')
     parser.add_argument('-a', default='', nargs='?')
     return parser
 
-
 def main():
-
+    """Загрузка параметров командной строки, если нет параметров, то задаём значения по умоланию"""
     parser = create_arg_parser()
     namespace = parser.parse_args(sys.argv[1:])
     listen_address = namespace.a
     listen_port = namespace.p
 
     if not 1023 < listen_port < 65536:
-        SERVER_LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта '
-                               f'{listen_port}. Допустимы адреса с 1024 до 65535.')
+        LOGGER.critical(f'Попытка запуска сервера с указанием неподходящего порта {listen_port}. '
+                        f'Допустимы адреса с 1024 до 65535.')
         sys.exit(1)
-    SERVER_LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port},'
-                       f'Адрес, с которого принимаются подключения: {listen_address}. '
-                       f'Если адрес не указан, принимаются соединения с любых адресов.')
-
-    # try:
-    #     if '-p' in sys.argv:
-    #         listen_port = int(sys.argv[sys.argv.index('-p') + 1])
-    #     else:
-    #         listen_port = DEFAULT_PORT
-    #     if listen_port < 1024 or listen_port > 65535:
-    #         raise ValueError
-    # except IndexError:
-    #     print('После параметра -\'p\' необходимо указать номер порта.')
-    #     sys.exit(1)
-    # except ValueError:
-    #     print('В качестве порта может быть указано только число в диапазоне от 1024 до 65535.')
-    #     sys.exit(1)
-    #
-    # try:
-    #     if '-a' in sys.argv:
-    #         listen_address = sys.argv[sys.argv.index('-a') + 1]
-    #     else:
-    #         listen_address = ''
-    #
-    # except IndexError:
-    #     print('После параметра \'a\' необходимо указать адрес, который будет слушать сервер.')
-    #     sys.exit(1)
+    LOGGER.info(f'Запущен сервер, порт для подключений: {listen_port}, адрес,'
+                f' с которого принимаются подключения: {listen_address}. '
+                f'Если адрес не указан, принимаются соединения с любых адресов.')
 
     transport = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    transport.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     transport.bind((listen_address, listen_port))
 
     transport.listen(MAX_CONNECTIONS)
 
     while True:
         client, client_address = transport.accept()
-        SERVER_LOGGER.info(f'Установлено соединение с ПК {client_address}')
+        LOGGER.info(f'Установлено соедение с ПК {client_address}')
         try:
-            message_from_client = get_message(client)
-            SERVER_LOGGER.debug(f'Получено сообщение {message_from_client}')
-            # print(message_from_client)
-            response = process_client_message(message_from_client)
-            SERVER_LOGGER.info(f'Сформирован ответ клиенту {response}')
+            message_from_cient = get_message(client)
+            LOGGER.debug(f'Получено сообщение {message_from_cient}')
+            print(message_from_cient)
+            response = process_client_message(message_from_cient)
+            LOGGER.info(f'Cформирован ответ клиенту {response}')
             send_message(client, response)
-            SERVER_LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
+            LOGGER.debug(f'Соединение с клиентом {client_address} закрывается.')
             client.close()
         except json.JSONDecodeError:
-            SERVER_LOGGER.error(f'Не удалось декодировать JSON строку, полученную от клиента'
-                                f'{client_address}. Соединение закрывается.')
+            LOGGER.error(f'Не удалось декодировать Json строку, '
+                         f'полученную от клиента {client_address}. Соединение закрывается.')
             client.close()
-        except IncorrectDataReceivedError:
-            SERVER_LOGGER.error(f'От клиента {client_address} приняты некорректные данные.'
-                                f'Соединение закрывается.')
+        except IncorrectDataRecivedError:
+            LOGGER.error(f'От клиента {client_address} приняты некорректные данные. '
+                         f'Соединение закрывается.')
             client.close()
 
 
